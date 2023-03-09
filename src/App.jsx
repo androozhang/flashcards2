985c5061fba40fdeed92551ee349153892a74f52
Andrew Zhang <zandrew129@gmail.com>
2023-03-08 22:54:20 -0500

    Completed flashcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,92 @@
 import './App.css';
 import { useState } from 'react';
-import Card from './components/Card'
+import ReactCardFlip from "react-card-flip";
 
 const App = () => {
-  const cardData = [
-    {
-      id: 1,
-      question: "question1",
-      answer: "answer1",
-    },
-    {
-      id: 2,
-      question: "question2",
-      answer: "answer1",
-    },
-    {
-      id: 3,
-      question: "question3",
-      answer: "answer1",
-    },
-    {
-      id: 4,
-      question: "question4",
-      answer: "answer1",
-    },
-    {
-      id: 5,
-      question: "question5",
-      answer: "answer1",
-    
-    },
-    
-  ]
-
-  const nextQuestion = () => {
-    setQuestionNum(Math.floor(Math.random() * cardData.length));
-  }
+  var [count, setCount] = useState(0);
+  const [flip, setFlip] = useState(false);
+  
+  const handleNextCard = () => {
+    setCount(Math.floor(Math.random() * 9) + 1);
+    setFlip(false);
+  };
 
   return (
     <div className="App">
-      <h1>Flashcards</h1>
-      <h2>Number of cards: {cardData.length}</h2>
-      <div className="card" >
-      <Card cards={cardData}/>
+      <div>
+        <h2>
+          Droo Quiz
+        </h2>
       </div>
-      <button className='button' onClick={nextQuestion}>Next</button>
+        
+      <div>
+      <ReactCardFlip isFlipped={flip} 
+            flipDirection="vertical">
+            <div onClick={() => setFlip(!flip)} className='front'>
+                {cards[count].question}
+            </div>
+            <div onClick={() => setFlip(!flip)} className='back'>
+              {cards[count].answer}
+            </div>
+      </ReactCardFlip>
+      </div>
+      <button className="button" onClick={handleNextCard}>
+        Next Card
+      </button>
     </div>
   )
 }
 
+const cards = [
+  {
+    id: "1",
+    question: "What zodiac sign am I?",
+    answer: "Aquarius",
+  },
+  {
+    id: "2",
+    question: "Do I like pizza?",
+    answer: "Not really",
+  },
+  {
+    id: "3",
+    question: "Favorite color",
+    answer: "Blue is cool",
+  },
+  {
+    id: "4",
+    question: "Favorite time of day?",
+    answer: "3:12 PM",
+  },
+  {
+    id: "5",
+    question: "Go to meat at chipotle?",
+    answer: "Chicken",
+  },
+  {
+    id: "6",
+    question: "Favorite food truck?",
+    answer: "Jamaican",
+  },
+  {
+    id: "7",
+    question: "What time was I born?",
+    answer: "6:30 PM",
+  },
+  {
+    id: "8",
+    question: "How many siblings do I have?",
+    answer: "0",
+  },
+  {
+    id: "9",
+    question: "Favorite lays chip?",
+    answer: "Salt and vinegar",
+  },
+  {
+    id: "10",
+    question: "2+2?",
+    answer: "4",
+  }
+];
 export default App
